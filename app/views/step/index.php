0fef93c3147e982d6083e86dce5f90e1d64f0842
pochun840 <0840@kilews.com.tw>
2024-06-04 08:59:01 +0800

    修改step的copy 系統會自動帶入新的step_id

--- app/views/step/index.php
+++ app/views/step/index.php
@@ -78,10 +78,11 @@
         </div>
 
         <div class="buttonbox">
-            <input id="S3" name="Step_Manager_Submit" type="button" value="New" tabindex="1" onclick="document.getElementById('newstep').style.display='block'">
+
+            <input id="S3" name="Step_Manager_Submit" type="button" value="New" tabindex="1"  onclick="cound_step('new');" >
             <input id="S6" name="Step_Manager_Submit" type="button" value="Edit" tabindex="1">
-            <input id="S5" name="Step_Manager_Submit" type="button" value="Copy" tabindex="1" onclick="document.getElementById('copystep').style.display='block'">
-            <input id="S4" name="Step_Manager_Submit" type="button" value="Delete" tabindex="1" >
+            <input id="S5" name="Step_Manager_Submit" type="button" value="Copy" tabindex="1"  onclick="cound_step('copy');">
+            <input id="S4" name="Step_Manager_Submit" type="button" value="Delete" tabindex="1" onclick="cound_step('del');" >
         </div>
     </div>
 
@@ -100,10 +101,11 @@
                         <div class="row">
                             <div for="target-option" class="col-6 t1">Target Option :</div>
                             <div class="col-3 t2">
-                                <select id="Target_Option" class="col custom-file">
-                                    <option value="1">Torque</option>
-                                    <option value="2">Angle</option>
-                                    <option value="3">Delay Time</option>
+                                <select id="target_option" name="target_option" class="col custom-file">
+                                    <?php foreach($data['target_option'] as $key => $val){?>
+                                        <option value="<?php echo $key;?>"><?php echo $val;?></option>
+                                    <?php }?>
+                                    
                                 </select>
                             </div>
                         </div>
@@ -161,11 +163,11 @@
                             <div for="downshift" class="col-6 t1">Downshift:</div>
                             <div class="col t2" >
             			      	<div class="col-4 form-check form-check-inline">
-            					  <input class="form-check-input" type="radio" name="downshift_option" id="downshift_ON" value="1">
+            					  <input class="form-check-input" type="radio" name="downshift_option" id="downshift_ON" value="0" checked="checked">
             					  <label class="form-check-label" for="downshift_ON">ON</label>
             					</div>
             					<div class="form-check form-check-inline">
-            					  <input class="form-check-input" type="radio" name="downshift_option" id="downshift_OFF" value="2" checked="checked">
+            					  <input class="form-check-input" type="radio" name="downshift_option" id="downshift_OFF" value="1" >
             					  <label class="form-check-label" for="downshift_OFF">OFF</label>
             					</div>
                             </div>
@@ -192,7 +194,7 @@
                 </div>
 
                 <div class="modal-footer justify-content-center">
-                    <button id="" class="button-modal" onclick="add_member_user()" >Save</button>
+                    <button id="" class="button-modal" onclick="add_step()" >Save</button>
                     <button id="" class="button-modal" onclick="hideElementById('newstep');"  class="closebtn">Close</button>
                 </div>
             </div>
@@ -234,7 +236,7 @@
                 </div>
 
                 <div class="modal-footer justify-content-center">
-                    <button id="" class="button-modal" onclick="add_member_user()" >Save</button>
+                    <button id="copyButton" class="button-modal" onclick="copy_step_by_id_ajax()" >Save</button>
                     <button id="" class="button-modal" onclick="document.getElementById('copystep').style.display='none'" class="closebtn">Close</button>
                 </div>
             </div>
@@ -265,22 +267,11 @@ function highlight_row(tableId)
 // Get the modal
 var modal = document.getElementById('newstep');
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-
-
-// Button Return
-/*document.getElementById('back_btn').onclick = function()
-{
-    window.location.href = './tcc_sequence.html';
-    return;
-};*/
-
-
 
 
 var rows = document.getElementsByTagName("tr");
@@ -291,19 +282,200 @@ for (var i = 0; i < rows.length; i++) {
             cells[0].addEventListener("click", function() {
            
                 var stepid   = cells[0] ? (cells[0].textContent || cells[0].innerText) : null;
-                //var seqname = cells[1] ? (cells[1].textContent || cells[1].innerText) : null;
                 localStorage.setItem("stepid", stepid);
             });
         }
     })(rows[i]);
 }
 
-function del_stepid(step_id){
-    var jobid ='<?php echo $data['job_id']?>';
-    var seqid ='<?php echo $data['seq_id']?>';
 
-    if(jobid){
-        //進行ajax del 
+function cound_step(argument){
+
+    var stepid = readFromLocalStorage("stepid");
+    if(argument == 'del'){
+        del_stepid(stepid);
+    }
+
+    if(argument =="copy" && stepid != null){
+        copy_step(stepid);
+    }
+
+    if(argument =="new"){
+        create_step();
+    }
+
+
+    /*if(argument =="edit" && stepid != null){
+        edit_step(stepid);
+    }
+
+    if(argument =="new"){
+        create_step();
+    }
+
+    if(argument =="copy" && stepid != null){
+        copy_step(stepid);
+    }*/
+
+}
+
+function create_step() {
+    document.getElementById('newstep').style.display = 'block';
+
+    var targetoptionselect = document.getElementById('target_option');
+    targetoptionselect.addEventListener('change', function() {
+        var targetOptionValue = targetoptionselect.value;
+
+        var targetTorqueElement = document.getElementById('target_torque');
+        var hiTorqueElement = document.getElementById('hi_torque');
+        var loTorqueElement = document.getElementById('lo_torque');
+        var hiAngleElement = document.getElementById('hi_angle');
+        var loAngleElement = document.getElementById('lo_angle');
+        var rpmElement = document.getElementById('rpm');
+        var downshiftThresholdElement = document.getElementById('downshift_threshold');
+        var downshiftTorqueElement = document.getElementById('downshift_torque');
+        var downshiftRpmElement = document.getElementById('downshift_rpm');
+        var directionOptions = document.querySelectorAll('input[name="direction_option"]');
+        var downshiftOptions = document.querySelectorAll('input[name="downshift_option"]');
+
+        targetTorqueElement.disabled = false;
+        hiTorqueElement.disabled = false;
+        loTorqueElement.disabled = false;
+        hiAngleElement.disabled = false;
+        loAngleElement.disabled = false;
+        rpmElement.disabled = false;
+        downshiftThresholdElement.disabled = false;
+        downshiftTorqueElement.disabled = false;
+        downshiftRpmElement.disabled = false;
+        directionOptions.forEach(radioButton => radioButton.disabled = false);
+        downshiftOptions.forEach(radioButton => radioButton.disabled = false);
+
+        if (targetOptionValue == 2) {
+            document.querySelector('div[for="target-torque"]').textContent = "Target Delay Time  (kgf-cm)";
+            targetTorqueElement.disabled = true;
+            hiTorqueElement.disabled = true;
+            loTorqueElement.disabled = true;
+            hiAngleElement.disabled = true;
+            loAngleElement.disabled = true;
+            rpmElement.disabled = true;
+            downshiftThresholdElement.disabled = true;
+            downshiftTorqueElement.disabled = true;
+            downshiftRpmElement.disabled = true;
+            directionOptions.forEach(radioButton => radioButton.disabled = true);
+            downshiftOptions.forEach(radioButton => radioButton.disabled = true);
+            
+        } else if (targetOptionValue == 1) {
+            document.querySelector('div[for="target-torque"]').textContent = "Target Angle (kgf-cm)";
+        }
+    });
+
+    var downshiftOptionRadios = document.getElementsByName("downshift_option");
+
+    for (var i = 0; i < downshiftOptionRadios.length; i++) {
+        downshiftOptionRadios[i].addEventListener("change", function() {
+          
+            if (this.checked) {
+                document.querySelector('div[for="downshift-threshold"]').style.display = "none";
+                document.getElementById('downshift_threshold').style.display = "none";
+
+                document.querySelector('div[for="downshift-threshold"]').style.display = "none";
+                document.getElementById('downshift_threshold').style.display = "none";
+
+                document.querySelector('div[for="downshift-torque"]').style.display = "none";
+                document.getElementById('downshift_torque').style.display = "none";
+
+                document.querySelector('div[for="downshift-rpm"]').style.display = "none";
+                document.getElementById('downshift_rpm').style.display = "none";
+
+
+            }
+        });
+    }
+}
+
+
+function add_step(){
+    var target_option = document.getElementById('target_option').value;
+    var target_torque = document.getElementById('target_torque').value;
+    
+
+}
+
+function copy_step(stepid){
+    document.getElementById('copystep').style.display = 'block';   
+    copy_step_by_id();
+
+}
+
+document.getElementById("copyButton").addEventListener("click", function() {
+    copy_step_by_id();
+});
+
+function copy_step_by_id(){
+    var stepid = readFromLocalStorage("stepid");
+    var jobid = '<?php echo $data['job_id']?>';
+    var seqid = '<?php echo $data['seq_id']?>';
+    var seqidnew = '<?php echo $data['stepid_new']?>';
+    
+    document.getElementById('from_step_id').value = stepid;    
+    document.getElementById("to_step_id").value = seqidnew;
+
+
+}
+
+function copy_step_by_id_ajax(){
+    var stepid = readFromLocalStorage("stepid");
+    var jobid = '<?php echo $data['job_id']?>';
+    var seqid = '<?php echo $data['seq_id']?>';
+    var stepid_new  = '<?php echo $data['stepid_new']?>';
+    
+    if(stepid_new){
+        $.ajax({
+            url: "?url=Step/copy_step",
+            method: "POST",
+            data:{ 
+                jobid: jobid,
+                seqid: seqid,
+                stepid:stepid,
+                stepid_new: stepid_new
+            },
+            success: function(response) {
+                console.log( response);
+                alert(response);
+                history.go(0);
+            },
+            error: function(xhr, status, error) {
+                
+            }
+        });
+    }
+}
+
+
+function del_stepid(step_id){
+    var stepid = readFromLocalStorage("stepid");
+    var jobid = '<?php echo $data['job_id']?>';
+    var seqid = '<?php echo $data['seq_id']?>';
+    if(stepid) {
+
+        $.ajax({
+            url: "?url=Step/delete_step",
+            method: "POST",
+            data:{ 
+                stepid:stepid,
+                jobid:jobid,
+                seqid:seqid
+            },
+            success: function(response) {
+                console.log( response);
+                alert(response);
+                history.go(0);
+            },
+            error: function(xhr, status, error) {
+                
+            }
+        });
+
     }
 
 }
