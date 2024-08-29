4366c11e5a74fc04d163e61f1a57de4bb5e56a61
pochun840 <0840@kilews.com.tw>
2024-08-29 10:41:02 +0800

    update file

--- app/views/tool/index.php
+++ app/views/tool/index.php
@@ -2,106 +2,86 @@
 
 <div class="container-ms">
     <div class="w3-text-white w3-center">
-        <table class="no-border">
+        <table>
             <tr id="header">
-                <td width="100%"><h3><?php echo $text['tool_info'];?></h3></td>
-                <td><img src="./img/btn_home.png" style="margin-right: 10px" onclick="back()"></td>
+                <td width="100%">
+                    <h3><?php echo $text['tool']; ?></h3>
+                </td>
+                <td>
+                    <button class="w3-btn w3-round-large" style="height:50px;padding: 0" onclick="window.location.href='./?url=Dashboards'"> <img src="../public/img/btn_home.png"></button>
+                </td>
             </tr>
         </table>
     </div>
 
-    
     <div class="main-content">
         <div class="center-content">
-            <div class="tool-setting">
-                <div class="scrollbar" id="style-tool">
-                    <div class="force-overflow">
-                        <div class="col t1" style="padding-left: 3%;font-weight: bold; padding-top: 1%"><?php echo $text['tool_info'];?></div>
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['tool_type'];?>:</div>
-                            <div class="col t2">
-                                <div>──────────────────────────────────────</div>
-                            </div>
-                        </div>    
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['tool_sn'];?>:</div>
-                            <div class="col t2">
-                                <div>──────────────────────────────────────</div>
-                            </div>
-                        </div> 
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['calibration_value'];?>:</div>
-                            <div class="col t2">
-                                <div>──────────────────────────────────────</div>
-                            </div>
-                        </div>    
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['total_counts'];?>:</div>
-                            <div class="col t2">
-                                <div>──────────────────────────────────────</div>
-                            </div>
-                        </div>    
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['rpm'];?>:</div>
-                            <div class="col t2">
-                                <div>──────────────────────────────────────</div>
-                            </div>
-                        </div>    
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['torque'];?> (<?php echo $text['Nm'];?>):</div>
-                            <div class="col t2">
-                                <div>──────────────────────────────────────</div>
-                            </div>
-                        </div>    
-                        <div class="row t2">    
-                            <div class="col-4 t1"><?php echo $text['sw_version'];?>:</div>
-                            <div class="col t2">
-                                <div>──────────────────────────────────────</div>
-                            </div>
-                        </div>    
-                        
+            <div class="container" style="padding: 10px;border-radius: 5px ;box-shadow: 0px 3px 8px 0px rgba(0, 0, 0, 0.2);">
+                <div id="Tool_Setting">
+                        <h3 style="margin: 5px 3px 10px"><b><?php echo $text['tool_info'];?></b></h3>
+                        <div class="row  border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px" ><?php echo $text['tool_type'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px">---------</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['tool_sn'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px">---------</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['sw_version'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px">---------</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['maintain_counts'];?>:</div>
+                            <div class="col-2" style="font-size: 18px; margin: 5px 5px 5px">---------</div>&nbsp;
+                           
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['total_counts'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px">---------</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['tool_max_torque2'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px">---------</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['tool_max_speed'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px">---------</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['calibration_value'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px">---------</div>
+                        </div>
+
                         <hr>
-                        
-                        <div class="col t1" style="padding-left: 3%;font-weight: bold"><?php echo $text['controller_info'];?></div>            
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['controller_sn'];?>:</div>
-                            <div class="col t2">
-                                <div>──────────────────────────────────────</div>
-                            </div>
-                        </div>    
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['controller_version'];?>:</div>
-                            <div class="col t2">
-                                <div>──────────────────────────────────────</div>
-                            </div>
-                        </div>    
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['mcb_version'];?>:</div>
-                            <div class="col t2">
-                                <div>──────────────────────────────────────</div>
-                            </div>
-                        </div>    
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['image_version'];?>:</div>
-                            <div class="col t2">
-                                <div>──────────────────────────────────────</div>
-                            </div>
-                        </div>    
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['network_ip'];?>:</div>
-                            <div class="col t2">
-                                <div><?php echo $data['IP'];?></div>
-                            </div>
-                        </div>    
-                        <div class="row t2">
-                            <div class="col-4 t1"><?php echo $text['Mac'];?>:</div>
-                            <div class="col t2">
-                                <div><?php echo $data['MAC'];?></div>
-                            </div>
-                        </div>    
-                    </div>
-                </div>              
+
+                        <h3 style="margin: 5px 3px 10px"><b><?php echo $text['controller_info'];?></b></h3>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['controller_sn'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px">---------</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['controller_version'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px">---------</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['mcb_version'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px">---------</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['image_version'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px">---------</div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['network_ip'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px"><?php echo $data['IP']; ?></div>
+                        </div>
+                        <div class="row border-bottom">
+                            <div class="col-6" style="font-size: 18px; margin: 5px 10px 5px"><?php echo $text['Mac'];?>:</div>
+                            <div class="col" style="font-size: 18px; margin: 5px 5px 5px"><?php echo $data['MAC']; ?></div>
+                        </div>
+                </div>
             </div>
         </div>
     </div>
-</div>    
+</div>
